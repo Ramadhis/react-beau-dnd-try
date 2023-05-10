ff5088f653a209807ccf6d58f8ce80e92f5db694
Ramadhiansyah <100117230+Ramadhis@users.noreply.github.com>
2023-05-10 14:24:42 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import VerticalList from "./component/VerticalList";
+import HorizontalList from "./component/HorizontalList";
+function App() {
+  return (
+    <>
+      <HorizontalList></HorizontalList>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/HorizontalList.jsx
@@ -0,0 +1,172 @@
+import { useState, useEffect } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+import _ from "lodash";
+import "./horizontallist.css";
+import { v4 } from "uuid";
+
+const item = {
+  id: v4(),
+  height: "20px",
+  name: "tesName",
+};
+const item2 = {
+  id: v4(),
+  height: "60px",
+  name: "tesName2",
+};
+const item3 = {
+  id: v4(),
+  height: "40px",
+  name: "tesName3 ",
+};
+
+console.log(item);
+
+function VerticalList() {
+  const [text, setText] = useState("");
+  const [state, setstate] = useState({
+    todo: {
+      title: "todo",
+
+      items: [item, item2],
+    },
+    "in-progress": {
+      title: "in Progress",
+      items: [item3],
+    },
+    done: {
+      title: "Completed",
+      items: [],
+    },
+  });
+
+  //to fix error Unable to find draggable with id: without disabled strict mode
+  // const StrictModeDroppable = ({ children, ...props }) => {
+  //   const [enabled, setEnabled] = useState(false);
+
+  //   useEffect(() => {
+  //     const animation = requestAnimationFrame(() => setEnabled(true));
+
+  //     return () => {
+  //       cancelAnimationFrame(animation);
+  //       setEnabled(false);
+  //     };
+  //   }, []);
+
+  //   if (!enabled) {
+  //     return null;
+  //   }
+
+  //   return <Droppable {...props}>{children}</Droppable>;
+  // };
+
+  //to fix error Unable to find draggable with id: without disabled strict mode
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  //end to fix error Unable to find draggable with id: without disabled strict mode
+
+  const handleDragEnd = ({ destination, source }) => {
+    console.log("from", source);
+    console.log("to", destination);
+
+    //drag drop validation
+    if (!destination) {
+      console.log("not dropped in droppable");
+      return;
+    }
+
+    if (destination.index === source.index && destination.droppableId == source.droppableId) {
+      console.log("drop in same place");
+      return;
+    }
+    //creating a copy of item before removing it from state
+    const itemCopy = { ...state[source.droppableId].items[source.index] };
+    setstate((prev) => {
+      prev = { ...prev };
+      //remove from previous items array
+      prev[source.droppableId].items.splice(source.index, 1);
+      //adding to new items array location
+      prev[destination.droppableId].items.splice(destination.index, 0, itemCopy);
+      return prev;
+    });
+  };
+
+  const addItem = () => {
+    setstate((prev) => {
+      return {
+        ...prev,
+        todo: {
+          title: "todo",
+          items: [
+            {
+              id: v4(),
+              name: text,
+            },
+            ...prev.todo.items,
+          ],
+        },
+      };
+    });
+
+    setText("");
+  };
+
+  return (
+    <div>
+      <h1>Horizontal List</h1>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button onClick={addItem}>add</button>
+      </div>
+      <div className="App">
+        <DragDropContext onDragEnd={handleDragEnd}>
+          {_.map(state, (data, key) => {
+            return (
+              <div key={key} className="column">
+                <h3>{data.title}</h3>
+
+                {isMounted ? (
+                  <Droppable droppableId={key} direction="horizontal">
+                    {(provided, snapshot) => {
+                      // console.log(snapshot);
+                      return (
+                        <div className={"droppable-col"} ref={provided.innerRef} {...provided.droppableProps}>
+                          {data.items.map((val, index) => {
+                            return (
+                              <Draggable key={val.id} index={index} draggableId={val.id}>
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div className={`items ${snapshot.isDragging && "dragging"}`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                      {/* <div style={{ color: "white", padding: "2px", height: val.height, background: "orange" }}>asd</div> */}
+                                      {val.name}
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                ) : null}
+              </div>
+            );
+          })}
+        </DragDropContext>
+      </div>
+    </div>
+  );
+}
+
+export default VerticalList;
